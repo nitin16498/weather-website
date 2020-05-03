52e559689940badfe66e88473f5c3673bd2c18e2
nitin16498 <48838399+nitin16498@users.noreply.github.com>
2020-05-04 01:08:32 +0530

    Port change error

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const hbs = require("hbs");
 
 const app = express();
-const port = process.env.Port || 3000;
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
