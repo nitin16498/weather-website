6993b6da955039eb96ab3aa85421a06ee2834173
nitin16498 <48838399+nitin16498@users.noreply.github.com>
2020-05-04 01:23:28 +0530

    change in weather display

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -21,7 +21,11 @@ const forecast = (latitude, longitude, callback) => {
         body.daily.data[0].summary +
           " It is currently " +
           body.currently.temperature +
-          " degress out. There is a " +
+          " degress out. The high today is " +
+          body.daily.data[0].temperatureHigh +
+          " and a low of " +
+          body.daily.data[0].temperatureLow +
+          " degrees. There is a " +
           body.currently.precipProbability +
           "% chance of rain."
       );
