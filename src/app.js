305421b0c6ac5c5ba97172b906fc4ec0045d8192
nitin16498 <48838399+nitin16498@users.noreply.github.com>
2020-05-04 00:53:14 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const hbs = require("hbs");
 
 const app = express();
+const port = process.env.Port || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -62,9 +63,6 @@ app.get("/weather", (req, res) => {
         });
       }
 
-      //   console.log(location);
-      //   console.log(forecastData);
-
       return res.send({
         address,
         location,
@@ -72,11 +70,6 @@ app.get("/weather", (req, res) => {
       });
     });
   });
-
-  //   res.send({
-  //     forecast: "It is snowing",
-  //     location: req.query.address,
-  //   });
 });
 
 app.get("/help/*", (req, res) => {
@@ -105,6 +98,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
